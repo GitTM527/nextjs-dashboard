138f9b163f103f91be4df1bedf776e8228575132
Ahkuemokhan Oluwatobi <oluwatobi.akhuemokhan@yahoo.com>
2025-11-28 19:05:23 +0100

    errors updated with Eslint-CLI

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -44,7 +44,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     if (!validatedFields.success) {
         return {
             errors: validatedFields.error.flatten().fieldErrors,
-            message: 'Missing Fields to Create Invoice.',
+            message: 'Missing Fields. Failed to Create Invoice.',
         };
     }
 
@@ -53,7 +53,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-   try { await sql`
+   // Insert data into the database
+    try { await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
@@ -69,14 +70,26 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices');
 } 
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+    id: string, 
+    prevState: State,
+    formData: FormData,
+) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
-const amountInCents = amount * 100;
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = amount * 100;
 
    try { await sql`
     UPDATE invoices
